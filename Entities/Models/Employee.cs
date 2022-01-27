69d56495783451755eae0ecae4c3b3b8bbe8e1bd
Oussama Moutafatin <moutafatin3@gmail.com>
2022-01-27 14:51:43 +0100

    Add Entities class library, and create company and employee models class

---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Employee
+{
+    [Column("EmployeeId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Employee name is required field.")]
+    [MaxLength(30, ErrorMessage = "Max length for the name is 30 characters.")]
+
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Age is a required field.")]
+
+    public int Age { get; set; }
+
+    [Required(ErrorMessage = "Position name is required field.")]
+    [MaxLength(60, ErrorMessage = "Max length for the position is 20 characters.")]
+
+    public string? Position { get; set; }
+
+    [ForeignKey(nameof(Company))]
+    public Guid CompanyId { get; set; }
+    public Company? Company { get; set; }
+}
